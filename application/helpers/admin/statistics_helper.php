5209cd106c19d706e0a55395ae7bf9c9375666cc
DelphiQin <delphiqin@gmail.com>
2016-09-03 13:50:51 +0800

    Insert 40px hight space between answer and graph

--- application/helpers/admin/statistics_helper.php
+++ application/helpers/admin/statistics_helper.php
@@ -3655,7 +3655,7 @@ class statistics_helper {
                             break;
                         case 'pdf':
 
-                            $this->pdf->Image($tempdir."/".$cachefilename, 0, 70, 180, 0, '', Yii::app()->getController()->createUrl("admin/survey/sa/view/surveyid/".$surveyid), 'B', true, 150,'C',false,false,0,true);
+                            $this->pdf->Image($tempdir."/".$cachefilename, 0, 110, 180, 0, '', Yii::app()->getController()->createUrl("admin/survey/sa/view/surveyid/".$surveyid), 'B', true, 150,'C',false,false,0,true);
 
                             break;
                         case 'html':
